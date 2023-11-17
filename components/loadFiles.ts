2892c2677bb96276062dc1bb333ffafbf87ab500
SatooRu65536 <SatooRu65536@users.noreply.github.com>
2023-11-17 16:52:20 +0000

    format: Apply Prettier Change

--- components/loadFiles.ts
+++ components/loadFiles.ts
@@ -20,7 +20,7 @@ export function getTopImages(): Image[] {
 function parseMetaTag(
   tags: null | string[],
   key: string,
-  defaultValue: string
+  defaultValue: string,
 ) {
   const tag = tags?.filter((tag) => tag.startsWith(`${key}:`))[0];
   return tag ? tag.replace(`${key}:`, "") : defaultValue;
@@ -43,7 +43,7 @@ export function getPages(): Page[] {
     const path_ = parseMetaTag(
       meta.tags,
       "path",
-      `/${meta.title.toLowerCase()}`
+      `/${meta.title.toLowerCase()}`,
     );
 
     return {
@@ -90,7 +90,7 @@ export function getPosts(): Post[] {
 
   const filteredPosts = pages.filter((page) => page.meta.title !== "README");
   filteredPosts.sort((a, b) =>
-    a.meta.created_at > b.meta.created_at ? -1 : 1
+    a.meta.created_at > b.meta.created_at ? -1 : 1,
   );
 
   return filteredPosts;
