11688e7c394c6402b1e1dea912d07d8023ddcb71
SatooRu65536 <ryuto4619@gmail.com>
2023-12-15 15:53:36 +0900

    add: /products にpagenation追加

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,49 +1,14 @@
-import Card from "./_components/card";
-import { getPages, getProducts } from "@/components/loadFiles";
-import { css } from "@/styled-system/css";
+"use client";
 
-export default function Products() {
-  const products = getProducts();
-  const publishedProducts = products.filter((product) => product.published);
-  const pages = getPages();
-  const productsPage = pages.find((page) => page.path === "/products")!;
+import { useRouter } from "next/navigation";
+
+export default function RedirectToPosts() {
+  const router = useRouter();
+  router.push("/products/1");
 
   return (
-    <main
-      className={css({
-        padding: "80px 20px",
-      })}
-    >
-      <div
-        className={css({
-          marginInline: "auto",
-          width: "100%",
-          maxWidth: "800px",
-        })}
-      >
-        <h1
-          className={css({
-            fontSize: "2rem",
-            fontWeight: 700,
-            marginBottom: "1rem",
-            borderBottom: "1px solid black",
-          })}
-        >
-          {productsPage.meta.title}
-        </h1>
-
-        <section
-          className={css({
-            display: "grid",
-            gap: "20px",
-            gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
-          })}
-        >
-          {publishedProducts.map((product) => (
-            <Card key={product.id} product={product} />
-          ))}
-        </section>
-      </div>
+    <main>
+      <p>/products/1 へ移動します</p>
     </main>
   );
 }
