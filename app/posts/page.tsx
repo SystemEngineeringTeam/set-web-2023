f0b2d29411a76556e9577daafb36a74efbb917ab
SatooRu65536 <ryuto4619@gmail.com>
2023-12-15 17:35:56 +0900

    Revert "fix: フロント側の処理をuseEffectで囲った"
    This reverts commit bf9769454dd2595111f128ba3c6e75d85f35f337.

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,14 +1,10 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
 
 export default function RedirectToPosts() {
   const router = useRouter();
-
-  useEffect(() => {
-    router.push("/posts/1");
-  }, [router]);
+  router.push("/posts/1");
 
   return (
     <main>
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,14 +1,10 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
 
 export default function RedirectToPosts() {
   const router = useRouter();
-
-  useEffect(() => {
-    router.prefetch("/products/1");
-  }, [router]);
+  router.push("/products/1");
 
   return (
     <main>
