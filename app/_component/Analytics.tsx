2cd9e1994e3fc316fc37767823cec5eef476073f
harutiro <harutiro2027@outlook.jp>
2024-04-16 22:27:24 +0900

    update: yarn fixした対応

--- app/_component/Analytics.tsx
+++ app/_component/Analytics.tsx
@@ -1,16 +1,16 @@
-"use client";
+'use client';
 
-import { logEvent } from "firebase/analytics";
-import { usePathname } from "next/navigation";
-import { useEffect } from "react";
-import { analytics } from "../firebase";
+import { logEvent } from 'firebase/analytics';
+import { usePathname } from 'next/navigation';
+import { useEffect } from 'react';
+import { analytics } from '../firebase';
 
 export default function Analytics() {
   const pathname = usePathname();
 
   useEffect(() => {
     if (!analytics) return;
-    logEvent(analytics, "page_view", {
+    logEvent(analytics, 'page_view', {
       page_location: pathname,
     });
   }, [pathname]);
--- app/firebase.ts
+++ app/firebase.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { getAnalytics } from "firebase/analytics";
-import { initializeApp } from "firebase/app";
+import { getAnalytics } from 'firebase/analytics';
+import { initializeApp } from 'firebase/app';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -11,7 +11,7 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
